cf408c5da869d898f45551b2d43e71d2810be0ff
block-hac <cn.corestar@gmail.com>
2021-09-07 21:07:39 +0800

    initial

---
+++ README.md
@@ -0,0 +1,12 @@
+## Author
+blockhac
+## 📸 ScreenShots
+
+| 1 | 2|
+|------|-------|
+|<img src="ss/2.png" width="400">|<img src="ss/3.png" width="400">|
+
+| 3 | 4|
+|------|-------|
+|<img src="ss/4.png" width="400">|<img src="ss/5.png" width="400">|
+
---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_foodybite/screens/home.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  PageController _pageController;
+  int _page = 0;
+
+  List icons = [
+    Icons.home,
+    Icons.label,
+    Icons.add,
+    Icons.notifications,
+    Icons.person,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        physics: NeverScrollableScrollPhysics(),
+        controller: _pageController,
+        onPageChanged: onPageChanged,
+        children: List.generate(5, (index) => Home()),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            SizedBox(width: 7),
+            buildTabIcon(0),
+            buildTabIcon(1),
+            buildTabIcon(2),
+            buildTabIcon(3),
+            buildTabIcon(4),
+            SizedBox(width: 7),
+          ],
+        ),
+        color: Theme.of(context).primaryColor,
+        shape: CircularNotchedRectangle(),
+      ),
+      floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        elevation: 10.0,
+        child: Icon(
+          Icons.add,
+        ),
+        onPressed: () => _pageController.jumpToPage(2),
+      ),
+    );
+  }
+
+  void navigationTapped(int page) {
+    _pageController.jumpToPage(page);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      this._page = page;
+    });
+  }
+
+  buildTabIcon(int index) {
+    if (index == 2) {
+      return IconButton(
+        icon: Icon(
+          icons[index],
+          size: 24.0,
+          color: Colors.transparent,
+        ),
+        onPressed: null,
+      );
+    } else {
+      return IconButton(
+        icon: Icon(
+          icons[index],
+          size: 24.0,
+        ),
+        color: _page == index
+            ? Theme.of(context).accentColor
+            : Theme.of(context).textTheme.caption.color,
+        onPressed: () => _pageController.jumpToPage(index),
+      );
+    }
+  }
+}
